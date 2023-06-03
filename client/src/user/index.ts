ffc3d227c97925934535b312f07d12691b6268c9
Kajal Arora <a.kajal1405@gmail.com>
2023-06-04 03:06:19 +0530

    add email and password parsing logic

--- client/src/user/index.ts
+++ client/src/user/index.ts
@@ -5,15 +5,69 @@ enum USER_LOGIN_CMDS {
   sign_in = "login",
 }
 
+function getProcessArgValue(
+  userArgs: string[],
+  matchArg: "--email" | "--password"
+): string | null {
+  const emailArgIndex = userArgs.indexOf(matchArg);
+  for (let i = emailArgIndex + 1; i < userArgs.length; i++) {
+    //check if next value exists example --passsword "test" --email
+    if (!userArgs[i]) {
+      console.error("Please provide username and password.");
+      process.exit(1);
+    }
+    // check if value is present in next arg or is again a parameter. Eg --email --password "test"
+    else if (userArgs[i].includes("--")) {
+      console.error("Please provide username and password.");
+      process.exit(1);
+    } else {
+      return userArgs[i];
+    }
+  }
+  return null;
+}
+
+enum ERRORS {
+    /* error codes for process exit */
+}
+
+function getEmailAndPassword(): { email: string, password: string } {
+  let email: string, password: string;
+
+  //get args after user action
+  const userValues =
+    process.argv.slice(process.argv.indexOf(USER_LOGIN_CMDS.sign_up) + 1) ||
+    process.argv.slice(process.argv.indexOf(USER_LOGIN_CMDS.sign_in) + 1);
+
+  if (userValues.length === 0) {
+    console.error("Email and password not present.");
+    process.exit(1);
+  }
+
+  email = getProcessArgValue(userValues, "--email")!;
+  password = getProcessArgValue(userValues, "--password")!;
+
+  return { email, password };
+}
+
+function handleSignUpUser() {
+  const { email, password } = getEmailAndPassword();
+
+  //   const { email, password } = getEmailAndPassword();
+  console.log({ email, password });
+}
+
+function handleSignInUser() {}
+
 async function manageUserAuthActions() {
   let userAction = process.argv[2];
-  
 
   switch (userAction) {
     case USER_LOGIN_CMDS.sign_up:
-      {
-        console.log(process.argv)
-      }
+      handleSignUpUser();
+      break;
+    case USER_LOGIN_CMDS.sign_in:
+      handleSignInUser();
       break;
   }
 }
